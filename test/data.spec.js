68199e4b1eb00246e766bcf8801804c1415a2b9e
MiriamDiaz2306 <111524376+MiriamDiaz2306@users.noreply.github.com>
2022-11-28 11:29:29 -0500

    cambio equis

--- test/data.spec.js
+++ test/data.spec.js
@@ -17,12 +17,11 @@ describe('filtro de caracteristicas', () => {
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+//describe('anotherExample', () => {
+ // it('is a function', () => {
+   // expect(typeof anotherExample).toBe('function');
+//  });
 
   it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+  expect(anotherExample()).toBe('OMG');
 });
