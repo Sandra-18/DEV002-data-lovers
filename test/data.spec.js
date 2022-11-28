31fb851940d4cd9505eb0319710251a52bca82db
MiriamDiaz2306 <111524376+MiriamDiaz2306@users.noreply.github.com>
2022-11-28 11:39:13 -0500

    solucion de conflictos

--- test/data.spec.js
+++ test/data.spec.js
@@ -34,15 +34,7 @@ describe('filtro de Humanos', () => {
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
 
 
 
